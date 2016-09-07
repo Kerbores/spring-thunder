995db4dafa8d50dd7c1f2b7dd6a03e1481ca88d5
王贵源 <kerbores@zhcs.club>
2016-09-08 00:29:41 +0800

    init

---
+++ spring-thunder/src/main/java/club/zhcs/thunder/controller/TestController.java
@@ -0,0 +1,36 @@
+package club.zhcs.thunder.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Kerbores(kerbores@gmail.com)
+ *
+ * @project spring-thunder
+ *
+ * @file TestController.java
+ *
+ * @description // TODO write some description of this class
+ *
+ * @time 2016年9月7日 下午11:50:43
+ *
+ */
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+	@RequestMapping("/json")
+	public @ResponseBody Map<String, Object> json() {
+
+		HashMap<String, Object> target = new HashMap<String, Object>();
+
+		target.put("msg", "Hello spring-mvc");
+
+		return target;
+	}
+
+}
---
+++ spring-thunder/src/main/java/club/zhcs/thunder/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author Kerbores(kerbores@gmail.com)
+ *
+ * @project spring-thunder
+ *
+ * @file package-info.java
+ *
+ * @description // TODO write some description of this class
+ *
+ * @time 2016年9月7日 下午11:50:22
+ *
+ */
+package club.zhcs.thunder;
