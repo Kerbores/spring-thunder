a0b677f14ceff41a839bdb38a50535e8d90b200b
王贵源 <kerbores@gmail.com>
2016-11-04 11:43:31 +0800

    shiro进来了

---
+++ spring-thunder/src/main/java/club/zhcs/thunder/biz/log/LoginLogService.java
@@ -0,0 +1,18 @@
+package club.zhcs.thunder.biz.log;
+
+import org.springframework.stereotype.Service;
+
+import club.zhcs.thunder.domain.log.LoginLog;
+import club.zhcs.titans.utils.biz.BaseService;
+
+/**
+ * 
+ * @author kerbores
+ *
+ * @email kerbores@gmail.com
+ *
+ */
+@Service
+public class LoginLogService extends BaseService<LoginLog> {
+
+}
---
+++ spring-thunder/src/main/java/club/zhcs/thunder/biz/log/OperationLogService.java
@@ -0,0 +1,18 @@
+package club.zhcs.thunder.biz.log;
+
+import org.springframework.stereotype.Service;
+
+import club.zhcs.thunder.domain.log.OperationLog;
+import club.zhcs.titans.utils.biz.BaseService;
+
+/**
+ * 
+ * @author kerbores
+ *
+ * @email kerbores@gmail.com
+ *
+ */
+@Service
+public class OperationLogService extends BaseService<OperationLog> {
+
+}
