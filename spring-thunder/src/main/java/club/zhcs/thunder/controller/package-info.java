048b2ccffaef108a168f241ce86d01c4c6122e42
kerbores <kerbores@gmail.com>
2016-11-17 07:52:59 +0800

    warning

--- spring-thunder/src/main/java/club/zhcs/thunder/controller/package-info.java
+++ spring-thunder/src/main/java/club/zhcs/thunder/controller/package-info.java
@@ -2,7 +2,6 @@
  * @author Kerbores(kerbores@gmail.com)
  * @project spring-thunder
  * @file package-info.java
- * @description // TODO write some description of this class
  * @time 2016年9月7日 下午11:50:32
  */
 package club.zhcs.thunder.controller;
--- spring-thunder/src/main/java/club/zhcs/thunder/package-info.java
+++ spring-thunder/src/main/java/club/zhcs/thunder/package-info.java
@@ -2,7 +2,6 @@
  * @author Kerbores(kerbores@gmail.com)
  * @project spring-thunder
  * @file package-info.java
- * @description // TODO write some description of this class
  * @time 2016年9月7日 下午11:50:22
  */
 package club.zhcs.thunder;
