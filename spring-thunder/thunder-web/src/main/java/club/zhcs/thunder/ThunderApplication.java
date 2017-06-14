d9d4aae17df2b6bf748ec870890d2b2343405ea2
Kerbores <Kerbores@gmail.com>
2017-06-14 10:48:32 +0800

    add:application

---
+++ spring-thunder/thunder-web/src/main/java/club/zhcs/thunder/ThunderApplication.java
@@ -0,0 +1,17 @@
+package club.zhcs.thunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kerbores
+ *
+ */
+@SpringBootApplication
+public class ThunderApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ThunderApplication.class, args);
+	}
+
+}
---
+++ spring-thunder/thunder-web/src/main/java/club/zhcs/thunder/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author kerbores
+ *
+ */
+package club.zhcs.thunder;
